c957cbaee93a4f2e5ebd2ad9104f035c3c38e482
Rio Noredjo <rio@noredjo.com>
2022-10-21 14:22:56 +0100

    Add/modified basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AccountComponent } from './components/account/account.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'account', component: AccountComponent },
-  { path: 'add-user', component: AccountComponent },
+  { path: 'add-user', component: AddUserComponent },
   { path: '**', redirectTo: 'home' },
 ];
 
